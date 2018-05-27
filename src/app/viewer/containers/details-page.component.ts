4bec4698b57ad2fb400441e7719b94b5441b8872
Kamil Neczaj <kneczaj@gmail.com>
2018-05-27 13:11:47 +0200

    [Viewer] Details view styling

--- src/app/viewer/containers/details-page.component.ts
+++ src/app/viewer/containers/details-page.component.ts
@@ -9,20 +9,35 @@ import {ActivatedRoute} from '@angular/router';
   template: `
     <div class="container">
       <div *ngIf="(video$ | async) as video">
-        <yv-video-player [video]="video"></yv-video-player>
-        <h2>{{video.title}}</h2>
-        <h4>Tags</h4>
-        <div>
-          <span *ngFor="let tag of video.tags">
-            {{tag}}
-          </span>
-        </div>
-        <h4>Description</h4>
-        <div>{{ video.description }}</div>
+        <section class="my-3">
+          <yv-video-player [video]="video"></yv-video-player>
+          <h2>{{video.title}}</h2>
+        </section>
+        <hr>
+        <section class="my-2">
+          <h4 class="mb-2">Tags</h4>
+          <div>
+            <span *ngFor="let tag of video.tags" class="badge">
+              {{tag}}
+            </span>
+          </div>
+        </section>
+        <hr>
+        <section class="my-2">
+          <h4 class="mb-2">Description</h4>
+          <div>{{ video.description }}</div>
+        </section>
       </div>
     </div>
   `,
-  styles: []
+  styles: [`
+    .badge {
+      color: #fff;
+      background-color: #777;
+      margin-right: 0.3rem;
+      cursor: default;
+    }
+  `]
 })
 export class DetailsPageComponent implements OnInit {
 
