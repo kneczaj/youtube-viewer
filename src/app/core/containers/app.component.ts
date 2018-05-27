d9e617fbaeff835cea410870594250aaac46292e
Kamil Neczaj <kneczaj@gmail.com>
2018-05-27 10:31:19 +0200

    [App] Header bar

--- src/app/core/containers/app.component.ts
+++ src/app/core/containers/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'yv-root',
   template: `
-    <router-outlet></router-outlet>
+    <header>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container justify-content-between">
+          <a href="" class="navbar-brand align-items-center">
+            <strong>Youtube Viewer</strong>
+          </a>
+        </div>
+      </div>
+    </header>
+    <main role="main">
+      <router-outlet></router-outlet>
+    </main>
   `,
   styles: []
 })
